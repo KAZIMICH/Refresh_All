76f5450e73cb351b9b864185bc067425669320fe
KAZIMICH <101597999+KAZIMICH@users.noreply.github.com>
2022-10-12 11:51:53 +0200

    Добавлен вывод открытых файлов перед обновлением

--- refresh_All.py
+++ refresh_All.py
@@ -35,7 +35,6 @@ if True:
     file_path_DB = r'\\192.168.1.98\electric group\T40B\03 ПРОЕКТ док T40B\011 Перечень оборудования' \
                    r'\01_База данных' \
                    r'\Т40В_БД Оборудования.xlsx '
-
     # Указываем пути к файлам для работы с NX
     file_path_importNX = r'\\192.168.1.98\electric group\T40B\03 ПРОЕКТ док T40B\02 РКД Т-40\05 ' \
                          r'NX\Import\Т40В-Кабельные ' \
@@ -48,51 +47,64 @@ if True:
                          r'журналы для NX import SIGNAL.xlsx '
     # список путей файлов для работы с NX
     NX_list = (file_path_importNX, file_path_NXPOWER, file_path_NXSIGNAL)
-
     # путь к папке ПДСП проекта Т40В
     path_folder = r'\\192.168.1.98\electric group\T40B\03 ПРОЕКТ док T40B\01 T40B ПДСП\04 Электрика'
-
     # список, хранящий имена файлов ВК
     file_list = []
-
     # список, хранящий пути файлов ВК
     path_list = []
 
 
-# функция получения списка имен файлов ВК
-def get_name_VK(path_folder):
-    for root, dirs, files in os.walk(path_folder):
+# cписок имен файлов ВК
+def files_list(i):
+    for root, dirs, files in os.walk(i):
         for file in files:
             if file.endswith('ВК.xlsx') and not file.startswith('~$') \
                     or file.endswith('ВК.xls') and not file.startswith('~$') \
                     or file.endswith('ВК.xlsm') and not file.startswith('~$'):
                 file_list.append(os.path.join(file))
+    if len(file_list) > 0:
+        print(f'В папке с проектом найдено {len(file_list)} файлов для обновления')
+        answer = dialog_yes_no('Вывести список файлов для обновления?\nВведите Y или N')
+        if answer == 'y':
+            print_list(file_list)
+            print('_' * 100)
+    else:
+        print(input('Список пуст. Что-то пошло не так! Перезапустите программу...'))
+        sys.exit()
     return file_list
 
 
+# список открытых файлов проекта
+def file_list_open(i):
+    files_open = files_path(path_folder)
+    for j in files_open:
+        file_check(j)
+
+
 # Функция проверки дублей файлов
-def double(file_list):
+def double(i):
     visited = set()
-    dup = [x for x in file_list if x in visited or (visited.add(x) or False)]
+    dup = [x for x in i if x in visited or (visited.add(x) or False)]
     if len(dup) > 0:
-        for i in dup:
-            print(f'Документ {i} присутствует несколько раз')
+        for j in dup:
+            print(f'Документ {j} присутствует несколько раз')
         print(input('Удалите неактуальный файл(ы) и перезапустите приложение'))
         sys.exit()
 
 
-# Функция получения списка путей к файлам ВК
-def get_path_VK(folder_path):
-    for root, dirs, files in os.walk(folder_path):
+# получение списка путей к файлам ВК
+def files_path(i):
+    for root, dirs, files in os.walk(i):
         for file in files:
-            if file.endswith('ВК.xlsx') and not '~$' in file  \
+            if file.endswith('ВК.xlsx') and not '~$' in file \
                     or file.endswith('ВК.xls') and not '~$' in file \
                     or file.endswith('ВК.xlsm') and not '~$' in file:
                 path_list.append(os.path.join(root, file))
     return path_list
 
 
-# Функция проверки файла на присутствие и незанятость
+# Проверка файла на присутствие и незанятость
 def file_check(file):
     valid = False
     while not valid:
@@ -108,31 +120,13 @@ def file_check(file):
             print('Файл', file, 'был перемещен или удален')
             print(input('Для подтверждения нажмите Enter.\n'))
             valid = False
-    # return valid
+    return valid
 
 
-# Функция печати списков
-def print_list(lst):
-    for i in lst:
-        print(i)
-
-
-# Функция обновления файлов excel
-def refresh_files(path_list):
-    counter = 1
-    for i in path_list:
-        file_check(i)
-        print("Обновление файла", counter, "в списке")
-        print(i)
-        wb = excel.Workbooks.open(i)
-        wb.Application.DisplayAlerts = False
-        excel.Visible = False
-        wb.RefreshAll()
-        wb.Save()
-        wb.Close()
-        excel.Quit()
-        counter += 1
-        print('Файл', counter - 1, 'обновлен')
+# печать списков
+def print_list(i):
+    for j in i:
+        print(j)
 
 
 # открытие / обоновление / сохранение / обновление файла Базы данных
@@ -151,13 +145,33 @@ def open_DB(i):
     wb.Close()
     excel.Quit()
     print('Файл Базы данных обновлен')
+    print('_' * 100)
 
 
-# Функция проверки ввода пользовательских данных
-def dialog_yes_no(question):
+# обновление файлов excel
+def refresh_files(i):
+    counter = 1
+    for j in i:
+        file_check(j)
+        print("Обновление файла", counter, "в списке")
+        print(j)
+        wb = excel.Workbooks.open(j)
+        wb.Application.DisplayAlerts = False
+        excel.Visible = False
+        wb.RefreshAll()
+        wb.Save()
+        wb.Close()
+        excel.Quit()
+        counter += 1
+        print('Файл', counter - 1, 'обновлен')
+    print(f'Обновлено {len(path_list)} файлов в списке')
+
+
+# проверка ввода пользовательских данных
+def dialog_yes_no(i):
     while True:
         answers = {'yes': 1, 'y': 1, 'no': 0, 'n': 0}
-        print(question)
+        print(i)
         user_answer = input().lower()
         if user_answer in answers:
             return user_answer
@@ -166,22 +180,15 @@ def dialog_yes_no(question):
 
 
 # Тело программы
-open_DB(file_path_DB)
-print('_' * 100)
+# open_DB(file_path_DB)
 
-get_name_VK(path_folder)
+# files_list(path_folder)
+
+# file_list_open(path_folder)
 
 double(file_list)
 
-print(f'В папке с проектом найдено {len(file_list)} файлов для обновления')
-user_answer = dialog_yes_no('Вывести список файлов для обновления?\nВведите Y или N')
-
-if user_answer == 'y':
-    print_list(file_list)
-
-print('_' * 100)
-
-get_path_VK(path_folder)
+files_path(path_folder)
 
 print('Файлы готовы к обновлению')
 user_answer = dialog_yes_no('Обновить файлы?\nВведите Y или N')
@@ -197,8 +204,6 @@ else:
     print(input(f'Затраченное время = {int(totalTime)} секунд\n'))
     sys.exit()
 
-print(f'Обновлено {len(path_list)} файлов в списке')
-
 print('_' * 100)
 
 print('Обновление файлов взаимодействия с NX')
@@ -213,5 +218,4 @@ endTime = time.time()
 totalTime = endTime - startTime
 print('Программа завершена')
 print(input(f'Затраченное время = {int(totalTime)} секунд\n'))
-# input()
 sys.exit()
